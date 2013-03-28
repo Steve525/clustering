7f3bc13eb3632e9dbe9bd81c0886e3e7ea4f8c4c
Stephen Clarkson <clarksones@gmail.com>
2013-03-28 15:46:23 -0600

    Initial commit

---
+++ src/kmeans/KMeansLearner.java
@@ -0,0 +1,20 @@
+package kmeans;
+
+import toolkit.Matrix;
+import toolkit.SupervisedLearner;
+
+public class KMeansLearner extends SupervisedLearner {
+
+	@Override
+	public void train(Matrix features, Matrix labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void predict(double[] features, double[] labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
